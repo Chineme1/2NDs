36ef4520bdf2d70e9cac35b6e8ced1f64feb0e5c
Chineme1 <64276840+Chineme1@users.noreply.github.com>
2024-10-19 15:06:30 -0700

    Add files via upload

--- upload.py
+++ upload.py
@@ -1,23 +1,17 @@
-from flask import Flask, jsonify
 from scrape_main import MAIN_SCRAPER
 
-# Initialize Flask app
-app = Flask(__name__)
+def packIT(list):
+    finalArr = []
+    for i in range(len(list)):
+        finalArr.append({
+            'title': list[i][0],
+            'location': list[i][1],
+            'startTime': list[i][2],
+            'endTime': list[i][3]
+        })
+    
+    return finalArr
 
-# Define a route for the API endpoint
-@app.route('/events', methods=['GET'])
-def send_data():
-    # Sample data to be sent
-    data = {
-        'title': ["a","b","c","d"],
-        'location': 'UC 1223',
-        'start time': 'Software Developer',
-        'end time': 'New York'
-        
-    }
-     
-    return jsonify(data)
 
-# Run the app
-if __name__ == '__main__':
-    app.run(debug=True)
+
+
