445442e67f35ca5c26f70e20ea1ba0db84ffac01
Chineme1 <64276840+Chineme1@users.noreply.github.com>
2024-10-21 18:38:21 -0700

    Update scrape_main.py

--- scrape_main.py
+++ scrape_main.py
@@ -22,9 +22,12 @@ from selenium.webdriver.chrome.options import Options
 def MAIN_SCRAPER(url):
     # Initialize Selenium WebDriver (make sure to install the appropriate driver, e.g., ChromeDriver)
     options = Options()
-    options.headless = True  # Run Chrome in headless mode
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
+    #options.headless = True  # Run Chrome in headless mode
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--remote-debugging-port=9222')
 
     # Initialize the Selenium WebDriver with options
     driver = webdriver.Chrome(ChromeDriverManager(version="114.0.5735.90").install(), options=options)
