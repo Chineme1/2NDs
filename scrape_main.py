1a11bdc92a028a7fb38928b56dd4cd09d577b3cf
Chineme1 <64276840+Chineme1@users.noreply.github.com>
2024-10-23 12:05:45 -0700

    Update scrape_main.py

--- scrape_main.py
+++ scrape_main.py
@@ -14,8 +14,6 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 from total_scraper import scrape_all
 # from tinydb import TinyDB, Query
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.options import Options
 
 
 
@@ -23,14 +21,9 @@ def MAIN_SCRAPER(url):
     # Initialize Selenium WebDriver (make sure to install the appropriate driver, e.g., ChromeDriver)
     options = Options()
     #options.headless = True  # Run Chrome in headless mode
-    options.add_argument('--headless')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-    options.add_argument('--disable-gpu')
-    options.add_argument('--remote-debugging-port=9222')
 
     # Initialize the Selenium WebDriver with options
-    driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+    driver = webdriver.Chrome()
 
     # Open the webpage
     driver.get(url)
