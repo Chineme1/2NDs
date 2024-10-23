72d6bbe5ef8bd3901abafb0cb3b0315bd6f11fa8
Chineme1 <64276840+Chineme1@users.noreply.github.com>
2024-10-23 15:22:17 -0700

    Update scrape_main.py

--- scrape_main.py
+++ scrape_main.py
@@ -29,7 +29,7 @@ def MAIN_SCRAPER(url):
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-dev-shm-usage')
-    driver = webdriver.Chrome(service=Service(ChromeDriveManager().install()), options=chrome_options)
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 
     # Open the webpage
     driver.get(url)
