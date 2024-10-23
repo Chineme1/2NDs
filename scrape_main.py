f128471099dc912f2f7e154a0aba0f144a12c972
Chineme1 <64276840+Chineme1@users.noreply.github.com>
2024-10-23 15:12:51 -0700

    Update scrape_main.py

--- scrape_main.py
+++ scrape_main.py
@@ -25,6 +25,7 @@ def MAIN_SCRAPER(url):
     #options.headless = True  # Run Chrome in headless mode
 
     # Initialize the Selenium WebDriver with options
+    chrome_options = Options()
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-dev-shm-usage')
