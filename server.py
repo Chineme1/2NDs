5949149e62500e53bcfba65b504cdc631426e5aa
Chineme1 <64276840+Chineme1@users.noreply.github.com>
2024-10-24 05:37:54 -0700

    Update server.py

--- server.py
+++ server.py
@@ -10,11 +10,9 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def send_data():
     # Sample data to be sent
-    print('req')
     data = packIT(MAIN_SCRAPER('https://umdearborn.campuslabs.com/engage/events'))
-    print('data')
     return jsonify(data)
 
 # Run the app
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
