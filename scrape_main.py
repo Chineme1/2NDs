bb417b667c57b42e698fca4fb98c8c08d036ab1a
Chineme1 <64276840+Chineme1@users.noreply.github.com>
2024-10-24 06:35:32 -0700

    Update scrape_main.py

--- scrape_main.py
+++ scrape_main.py
@@ -29,6 +29,7 @@ def MAIN_SCRAPER(url):
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument("--disable-gpu")
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 
     # Open the webpage
