601271d2f72631d19ed373637505a50b250b10b9
Chineme1 <64276840+Chineme1@users.noreply.github.com>
2024-10-24 05:40:01 -0700

    Update server.py

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ from scrape_main import MAIN_SCRAPER
 app = Flask(__name__)
 
 # Define a route for the API endpoint
-@app.route('/', methods=['GET'])
+@app.route('/')
 def send_data():
     # Sample data to be sent
     data = packIT(MAIN_SCRAPER('https://umdearborn.campuslabs.com/engage/events'))
