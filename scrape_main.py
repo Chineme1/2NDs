c50e3a1c9798ac43e93ed572b8201bdc2ca35365
Chineme1 <64276840+Chineme1@users.noreply.github.com>
2024-10-24 07:12:10 -0700

    Update scrape_main.py

--- scrape_main.py
+++ scrape_main.py
@@ -25,11 +25,12 @@ def MAIN_SCRAPER(url):
     #options.headless = True  # Run Chrome in headless mode
 
     # Initialize the Selenium WebDriver with options
-    chrome_options = Options()
+    chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_argument("--disable-gpu")
+    chrome_options.binary_location = "/usr/bin/google-chrome"
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 
     # Open the webpage
